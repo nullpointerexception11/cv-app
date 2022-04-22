06e4270f0dca3b0bb68b4d67a2c37179fc2bc9da
lostcode <lostcoding11@gmail.com>
2022-04-22 16:14:28 +0300

    page-section created

---
+++ src/components/page-section/page-section.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PageSection = () => {
+  return (
+    <>
+      <section class="copyright">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12 text-center">
+              Copyright 2014 | All Rights Reserved -- Template by{" "}
+              <a href="http://webThemez.com">WebThemez.com</a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <a href="#top" class="topHome">
+        <i class="fa fa-chevron-up fa-2x"></i>
+      </a>
+    </>
+  );
+};
+
+export default PageSection;
