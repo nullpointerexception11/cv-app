f372c5b5a9f122e1b5766c5f1ab5533bc01b015f
lostcode <lostcoding11@gmail.com>
2022-04-24 19:08:37 +0300

    page section created

--- src/components/page-section/page-section.js
+++ src/components/page-section/page-section.js
@@ -3,18 +3,18 @@ import React from "react";
 const PageSection = () => {
   return (
     <>
-      <section class="copyright">
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-12 text-center">
+      <section className="copyright">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 text-center">
               Copyright 2014 | All Rights Reserved -- Template by{" "}
               <a href="http://webThemez.com">WebThemez.com</a>
             </div>
           </div>
         </div>
       </section>
-      <a href="#top" class="topHome">
-        <i class="fa fa-chevron-up fa-2x"></i>
+      <a href="#top" className="topHome">
+        <i className="fa fa-chevron-up fa-2x"></i>
       </a>
     </>
   );
