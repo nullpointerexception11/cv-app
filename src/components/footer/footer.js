257b0d7e1554afa85fc5dcfe67dbea36148d8003
lostcode <lostcoding11@gmail.com>
2022-04-24 19:08:27 +0300

    footer is ok

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,16 +3,16 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-    <div class="container">
-        <div class="social text-center">
-            <a href="#"><i class="fa fa-twitter"></i></a>
-            <a href="#"><i class="fa fa-facebook"></i></a>
-            <a href="#"><i class="fa fa-dribbble"></i></a>
-            <a href="#"><i class="fa fa-flickr"></i></a>
-            <a href="#"><i class="fa fa-github"></i></a>
+    <div className="container">
+        <div className="social text-center">
+            <a href="#"><i className="fa fa-twitter"></i></a>
+            <a href="#"><i className="fa fa-facebook"></i></a>
+            <a href="#"><i className="fa fa-dribbble"></i></a>
+            <a href="#"><i className="fa fa-flickr"></i></a>
+            <a href="#"><i className="fa fa-github"></i></a>
         </div>
 
-        <div class="clear"></div>
+        <div className="clear"></div>
     </div>
 </footer>  
 )
