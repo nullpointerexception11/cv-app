90874026dd33ee3977bd8b0b491cf0b773a3d807
lostcode <lostcoding11@gmail.com>
2022-04-24 19:07:50 +0300

    contact created

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -4,40 +4,40 @@ import Footer from '../footer/footer'
 const Contact = () => {
   return (
       
-    <section id="contactUs" class="page-section secPad">
-         <div class="container">
+    <section id="contactUs" className="page-section secPad">
+         <div className="container">
 
-        <div class="row">
-            <div class="heading text-center">
+        <div className="row">
+            <div className="heading text-center">
                 <h2>Let's Keep In Touch!</h2>
                 <p>Thank you for visiting out my profile. If you would like to get into contact with me, please fill out the form below.</p>
             </div>
         </div>
 
-        <div class="row mrgn30">
+        <div className="row mrgn30">
 
             <form method="post" action="" id="contactfrm" role="form">
 
-                <div class="col-sm-4">
-                    <div class="form-group">
+                <div className="col-sm-4">
+                    <div className="form-group">
                         <label for="name">Name</label>
-                        <input type="text" class="form-control" name="name" id="name" placeholder="Enter name" title="Please enter your name (at least 2 characters)"/>
+                        <input type="text" className="form-control" name="name" id="name" placeholder="Enter name" title="Please enter your name (at least 2 characters)"/>
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label for="email">Email</label>
-                        <input type="email" class="form-control" name="email" id="email" placeholder="Enter email" title="Please enter a valid email address"/>
+                        <input type="email" className="form-control" name="email" id="email" placeholder="Enter email" title="Please enter a valid email address"/>
                     </div>
                 </div>
-                <div class="col-sm-4">
-                    <div class="form-group">
+                <div className="col-sm-4">
+                    <div className="form-group">
                         <label for="comments">Comments</label>
-                        <textarea name="comment" class="form-control" id="comments" cols="3" rows="5" placeholder="Enter your message…" title="Please enter your message (at least 10 characters)"></textarea>
+                        <textarea name="comment" className="form-control" id="comments" cols="3" rows="5" placeholder="Enter your message…" title="Please enter your message (at least 10 characters)"></textarea>
                     </div>
-                    <button name="submit" type="submit" class="btn btn-lg btn-primary" id="submit">Submit</button>
-                    <div class="result"></div>
+                    <button name="submit" type="submit" className="btn btn-lg btn-primary" id="submit">Submit</button>
+                    <div className="result"></div>
                 </div>
             </form>
-            <div class="col-sm-4">
+            <div className="col-sm-4">
                 <h4>Address:</h4>
                 <address>
                     WebThemez Company<br/>
